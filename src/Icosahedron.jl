1f003a924716d4a2ca09190fab5c853120df585a
Jonas Seiler <Me@jonasseiler.de>
2025-06-22 22:43:13 +0200

    add face definitions to icosahedron

--- src/Icosahedron.jl
+++ src/Icosahedron.jl
@@ -55,9 +55,38 @@ function icosahedron_edges()::Vector{Tuple{Int,Int}}
     return edges
 end
 
+function icosahedron_faces()::Vector{Vector{Int}}
+    # double check, something still seems wrong
+    faces = [
+        [1, 2, 6],
+        [1, 2, 8],
+        [1, 11, 6],
+        [1, 8, 12],
+        [1, 12, 11],
+        [2, 6, 9],
+        [2, 10, 8],
+        [2, 9, 10],
+        [3, 5, 4],
+        [3, 4, 7],
+        [3, 12, 5],
+        [3, 7, 11],
+        [3, 11, 12],
+        [4, 5, 10],
+        [4, 9, 7],
+        [4, 10, 9],
+        [5, 8, 10],
+        [5, 12, 8],
+        [6, 7, 9],
+        [6, 11, 7],
+    ]
+
+    return faces
+end
+
 function plot_icosahedron()
     vertices = icosahedron_vertices()
     edges = icosahedron_edges()
+    faces = icosahedron_faces()
 
-    plot_3D_object(vertices, edges)
+    plot_3D_object(vertices, edges,faces)
 end
