5b18d7d3a9253e052819be513159a39a77908c1d
Jonas Seiler <Me@jonasseiler.de>
2025-07-07 13:19:15 +0200

    add face parametrization of cube

--- src/Cube.jl
+++ src/Cube.jl
@@ -13,7 +13,7 @@ function cube_vertices()::Vector{Vector{Float64}}
     return vertices
 end
 
-function cube_edges()::Vector{(Int, Int)}
+function cube_edges()::Vector{Tuple{Int,Int}}
     edges = [
         (1, 2),
         (1, 3),
@@ -29,4 +29,26 @@ function cube_edges()::Vector{(Int, Int)}
         (7, 8)
     ]
     return edges
+end
+
+function cube_faces()::Vector{Vector{Int}}
+    faces = [
+        [1, 2, 4, 3],
+        [1, 5, 6, 2],
+        [1, 3, 7, 5],
+        [2, 6, 8, 4],
+        [3, 4, 8, 7],
+        [5, 7, 8, 6],
+    ]
+
+    return faces
+end
+
+function plot_cube()
+    vertices = cube_vertices()
+    edges = cube_edges()
+    faces = cube_faces()
+
+    plot_3D_object(vertices, edges, faces)
+
 end
