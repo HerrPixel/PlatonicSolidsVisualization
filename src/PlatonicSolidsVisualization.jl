053d0e06ecd7893f048cffac328f196ef44e354e
Jonas Seiler <Me@jonasseiler.de>
2025-07-07 13:19:02 +0200

    add the new platonic solids and export the specific rendering functions

--- src/PlatonicSolidsVisualization.jl
+++ src/PlatonicSolidsVisualization.jl
@@ -2,6 +2,12 @@ module PlatonicSolidsVisualization
 include("Visualizer.jl")
 include("Icosahedron.jl")
 include("Cube.jl")
+include("Dodecahedron.jl")
+include("Tetrahedon.jl")
 
 export plot_icosahedron
+export plot_cube
+export plot_tetrahedon
+export plot_dodecahedron
+export plot_platonic_solids
 end
